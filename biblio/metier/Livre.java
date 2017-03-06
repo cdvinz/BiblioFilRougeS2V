2b7710a405880f9de3a3408498c9e9f2b760571d
AFPA <AFPA@192.168.101.148>
2017-03-06 10:34:00 +0100

    essais

---
+++ biblio/metier/Livre.java
@@ -0,0 +1,26 @@
+//Source file: C:\\Users\\vincent\\workspaceJAVA\\biblio\\metier\\Livre.java
+
+package biblio.metier;
+
+
+/**
+ * Catalogue de livres
+ */
+public class Livre 
+{
+   private String isbn;
+   private String titre;
+   private TypeDonneesAnnee anneeParution;
+   private Integer nbPages;
+   private Auteur auteur[];
+   private Editeur editeur;
+   private Theme theme;
+   
+   /**
+    * @roseuid 494232F202DE
+    */
+   public Livre() 
+   {
+    
+   }
+}
