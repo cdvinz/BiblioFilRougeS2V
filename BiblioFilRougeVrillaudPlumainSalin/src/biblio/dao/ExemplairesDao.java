ae48058c77a2e5118c23c761f636bb6846443b03
VV <AFPA@MASTER>
2017-03-06 15:01:08 +0100

    ExemplaireDaorésolue

--- BiblioFilRougeVrillaudPlumainSalin/src/biblio/dao/ExemplairesDao.java
+++ BiblioFilRougeVrillaudPlumainSalin/src/biblio/dao/ExemplairesDao.java
@@ -1,31 +1,22 @@
 package biblio.dao;
 
-import java.text.ParseException;
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.GregorianCalendar;
-
-import biblioFR.metier.EmpruntEnCours;
 import biblioFR.metier.EnumStatusExemplaire;
 import biblioFR.metier.Exemplaire;
-//import biblioFR.metier.Livre;
-import static biblioFR.metier.Personne.sdf;
 
 /**
  * @author SALIN
  * 
  */
-public class ExemplairesDao throws throws ParseException
-{
+public class ExemplairesDao{
 
-
-	/**   Emule la Base de donn�e des Exemplaires de Livre */
+	/**   Emule la Base de donnée des Exemplaires **/
 
 	private static Exemplaire [] exemplairesDB = {  
 
-			new Exemplaire (1, sdf.parse ("12/01/2017"),EnumStatusExemplaire.DISPONIBLE, "1111111"),
-			new Exemplaire (2, sdf.parse ("10/02/2017"),EnumStatusExemplaire.PRETE, "0022222"),
-			new Exemplaire (3, sdf.parse ("01/03/2017"),EnumStatusExemplaire.SUPPRIME, "0033333")};
+			new Exemplaire (1, new GregorianCalendar (2017,0,25).getTime(),EnumStatusExemplaire.DISPONIBLE, "1111111"),
+			new Exemplaire (2, new GregorianCalendar (2017,1,10).getTime(),EnumStatusExemplaire.PRETE, "0022222"),
+			new Exemplaire (3, new GregorianCalendar (2017,2,13).getTime(),EnumStatusExemplaire.SUPPRIME, "0033333")};
 
 
 	/**
@@ -41,6 +32,6 @@ public class ExemplairesDao throws throws ParseException
 
 	public Exemplaire findByKey (int idExemplaire)
 	{
-		for (Exemplaire exemplaire : exemplairesDB) {if (exemplaire.getIdExemplaire() == idExemplaire )return exemplaire;return null;}
-	}
+		for (Exemplaire exemplaire : exemplairesDB) {
+			if (exemplaire.getIdExemplaire() == idExemplaire ) return exemplaire;}return null;}
 }
