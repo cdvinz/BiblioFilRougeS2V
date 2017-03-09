e8837472b0295c2dc08df1031b5c104598dce235
xav017 <xher@192.168.101.22>
2017-03-09 12:05:32 +0100

    version après recette

--- BiblioFilRougeVrillaudPlumainSalin/src/biblioFR/test/TestEmployeEnRetard.java
+++ BiblioFilRougeVrillaudPlumainSalin/src/biblioFR/test/TestEmployeEnRetard.java
@@ -3,6 +3,7 @@ package biblioFR.test;
 import java.util.GregorianCalendar;
 
 import biblioFR.dao.ExemplairesDao;
+import biblioFR.dao.UtilisateursDao;
 import biblioFR.metier.Adherent;
 import biblioFR.metier.Employe;
 import biblioFR.metier.EmpruntEnCours;
@@ -12,7 +13,8 @@ public class TestEmployeEnRetard {
 		ExemplairesDao ed2=new ExemplairesDao ();
 		Employe ad5=new Employe ("JOLY", "Angelina", new GregorianCalendar (1970,3,10).getTime(), "F", 5, "qwerty", "AJ","0669696969");
 		System.out.println(ad5+"\n");
-		EmpruntEnCours empc2=new EmpruntEnCours(ad5,ed2.findByKey(2),new GregorianCalendar (2017,0,25).getTime());
+		UtilisateursDao ud1 = new UtilisateursDao();
+		EmpruntEnCours empc2=new EmpruntEnCours(ud1.findByKey(3),ed2.findByKey(2),new GregorianCalendar (2017,0,25).getTime());
 		System.out.println("Emprunt en retard :\n"+empc2+"\n");
 		EmpruntEnCours empc3=new EmpruntEnCours(ad5,ed2.findByKey(1),new GregorianCalendar (2017,2,15).getTime());
 		System.out.println("Emprunt à l'heure :\n"+empc3+"\n");
--- BiblioFilRougeVrillaudPlumainSalin/src/biblioFR/test/TestEmployeTroisEmprunts.java
+++ BiblioFilRougeVrillaudPlumainSalin/src/biblioFR/test/TestEmployeTroisEmprunts.java
@@ -3,6 +3,7 @@ package biblioFR.test;
 import java.util.GregorianCalendar;
 
 import biblioFR.dao.ExemplairesDao;
+import biblioFR.dao.UtilisateursDao;
 import biblioFR.metier.Adherent;
 import biblioFR.metier.Employe;
 import biblioFR.metier.EmpruntEnCours;
@@ -11,14 +12,15 @@ public class TestEmployeTroisEmprunts {
 
 	public static void main(String[] args) {
 		ExemplairesDao ed2=new ExemplairesDao ();
-		Employe ad5=new Employe ("JOLY", "Angelina", new GregorianCalendar (1970,3,10).getTime(), "F", 5, "qwerty", "AJ","0669696969");
-		EmpruntEnCours empc2=new EmpruntEnCours(ad5,ed2.findByKey(2),new GregorianCalendar (2017,2,15).getTime());
+		//Employe ad5=new Employe ("JOLY", "Angelina", new GregorianCalendar (1970,3,10).getTime(), "F", 5, "qwerty", "AJ","0669696969");
+		UtilisateursDao ud1 = new UtilisateursDao();
+		EmpruntEnCours empc2=new EmpruntEnCours(ud1.findByKey(4),ed2.findByKey(2),new GregorianCalendar (2017,2,15).getTime());
 		System.out.println("Emprunt à l'heure :\n"+empc2+"\n");
-		EmpruntEnCours empc3=new EmpruntEnCours(ad5,ed2.findByKey(3),new GregorianCalendar (2017,2,15).getTime());
+		EmpruntEnCours empc3=new EmpruntEnCours(ud1.findByKey(4),ed2.findByKey(3),new GregorianCalendar (2017,2,15).getTime());
 		System.out.println("Emprunt à l'heure :\n"+empc3+"\n");
-		EmpruntEnCours empc1=new EmpruntEnCours(ad5,ed2.findByKey(1),new GregorianCalendar (2017,2,15).getTime());
+		EmpruntEnCours empc1=new EmpruntEnCours(ud1.findByKey(4),ed2.findByKey(1),new GregorianCalendar (2017,2,15).getTime());
 		System.out.println("Emprunt à l'heure :\n"+empc1+"\n");
-		EmpruntEnCours empc4=new EmpruntEnCours(ad5,ed2.findByKey(4),new GregorianCalendar (2017,2,15).getTime());
+		EmpruntEnCours empc4=new EmpruntEnCours(ud1.findByKey(4),ed2.findByKey(4),new GregorianCalendar (2017,2,15).getTime());
 		System.out.println("Emprunt à l'heure :\n"+empc4+"\n");
 	}
 }
